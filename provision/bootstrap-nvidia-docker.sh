00aaab767ce13e85905f9ec35fdfa465ec7f60ab
obitech <github@f0rkd.net>
2018-05-02 18:25:15 +0200

    changed nvidia repo link

--- provision/bootstrap-nvidia-docker.sh
+++ provision/bootstrap-nvidia-docker.sh
@@ -3,8 +3,6 @@
 ###################### STARTING NVIDIA-DOCKER INSTALL ######################
 ############################################################################
 
-RELEASE=xenial
-
 # If nvidia-docker 1.0 is installed: we need to remove it and all existing GPU containers
 docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
 sudo apt-get purge -y nvidia-docker
@@ -13,7 +11,7 @@ sudo apt-get purge -y nvidia-docker
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
   sudo apt-key add -
 
-curl -s -L https://nvidia.github.io/nvidia-docker/${RELEASE}/nvidia-docker.list | \
+curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu16.04/nvidia-docker.list | \
   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
 
